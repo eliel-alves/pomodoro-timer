606567d2105e996f69d38a9d2b491876aba6c016
Eliel Alves <elielalves.dev@gmail.com>
2024-06-05 12:37:29 -0300

    feat: add home and history pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+
 import { Home } from './pages/Home'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
